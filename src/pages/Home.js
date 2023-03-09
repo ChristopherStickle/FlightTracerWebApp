e3d89e85c514433cff955a4753cfb2aa87143ae0
Christopher Stickle <cstickle@oswego.edu>
2023-03-09 11:44:21 -0500

    Basic NavBar working. Home,About,Map pages added.

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'Center',
+                alignItems: 'Right',
+                height: '100vh'
+            }}
+        >
+            <h1>Welcome to AirTracer</h1>
+        </div>
+    );
+};
+
+export default Home;
