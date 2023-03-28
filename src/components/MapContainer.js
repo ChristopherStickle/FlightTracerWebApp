ff5c8b0dd94adbe4d02542fd8fe0958bc2a7af24
Christopher Stickle <cstickle@oswego.edu>
2023-03-28 09:23:16 -0400

    update from dev

---
+++ src/components/MapContainer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj';
+import 'ol/ol.css';
+
+class MapContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            map: null
+        }
+    }
+    componentDidMount() {
+        const map = new Map({
+            target: 'map',
+            layers: [
+                new TileLayer({
+                    source: new OSM()
+                })
+            ],
+            view: new View({
+                center: fromLonLat([-76.510498,43.455345]),
+                //center: fromLonLat([0,0]),
+                zoom: 15
+
+            })
+        });
+        this.setState({map: map});
+    }
+    render() {
+        return (
+            <div id="map" style={{width: '100%', height: '100vh'}}/>
+        );
+    }
+}
+export default MapContainer;
---
+++ src/components/Results-Popup.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Results-Popup.css";
+/*
+* This is the popup that will display the results of the search.
+* It will list the Flights delivered from the database.
+* Displaying this popup will be triggered by the search button on the search menu.
+*  components:
+*      - a close button that will close the popup
+*      - a load button that will load the flights into the map
+*      - a list of flights that will be displayed in a table
+*      - a checkbox next to each flight that will allow the user to select the flight to load
+*/
+const ResultsPopup = props => {
+    return (
+        <div className="ResultsPopup">
+            <div className="box">
+                <button className="btn-close" onClick={props.handleClose}>X</button>
+                <button className="btn-load">Load</button>
+                {props.content}
+            </div>
+        </div>
+    );
+}
+export default ResultsPopup;
