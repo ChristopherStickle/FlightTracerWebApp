6baf3ab56fde243b33b189291c4f60e01eafdf0a
Christopher Stickle <cstickle@oswego.edu>
2023-04-05 17:25:50 -0400

    resultsPopup now has selectable items.  FlightID is returned to be used for DB queries.

--- src/pages/findflights.js
+++ src/pages/findflights.js
@@ -6,47 +6,117 @@ import "./table.css"
 import airportData from "./airportsReduced.json";
 
 function Findflights (){
-
     //-----------------------------------------------------------------------------------------------------------------
-    // for the search menu |
-    //----------------------
+    // search menu functions & variables |
+    //------------------------------------
     const [formData, setFormData] = useState({});
-    const [resultTable, setResultTable] = useState([]);
-
     const handleFormSubmit = (formStates) => {
+        clearResultsTable();
         // set the form data to the formStates
         setFormData(formStates);
-        alert("Form Submitted" + JSON.stringify(formData, null, 2));
-        const query = formData;
+        alert("Form Submitted" + JSON.stringify(formStates, null, 2));
+        const query = formStates;
         fetch("http://localhost:51261/submitAirplaneTracer", {
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify(query)
         }).then(res=>res.json()).then((result)=>{
-            alert(JSON.stringify(result))
+            alert("Query response: "+JSON.stringify(result, null, 2));
+            loadResultsTable(result);
         });
-        setResultTable(resultTable => [...resultTable, formData])
         togglePopup();
     }
     //-----------------------------------------------------------------------------------------------------------------
-    // for the popup |
-    //----------------
+    // Results popup functions & variables |
+    //--------------------------------------
+    const [resultTable, setResultTable] = useState([]);
     const [showPopup, setShowPopup] = React.useState(false);
+    const [selectedFlights, setSelectedFlights] = useState([]);
     const togglePopup = () => {
         setShowPopup(!showPopup);
+        setSelectedFlights([]);
+    }
+    function clearResultsTable() {
+        setResultTable([]);
+    }
+    function handleCheck (e) {
+        // if checked then add to selectedFlights
+        // if unchecked then remove from selectedFlights
+        if (e.target.checked) {
+            setSelectedFlights(selectedFlights => [...selectedFlights, e.target.value]);
+        } else {
+            setSelectedFlights(selectedFlights.filter(flight => flight !== e.target.value));
+        }
+    }
+    function handleLoad () {
+        togglePopup();
+        // make an alert to show the selected flights and their values
+        alert("Selected flights: "+ selectedFlights);
+        // query the database for the selected flights
+        const query = selectedFlights;
+        fetch("http://localhost:51261/submitAirplaneTracer", {
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(query)
+        }).then(res=>res.json()).then((result)=>{
+            alert("Query response: "+JSON.stringify(result, null, 2));
+            // send the selected flights to the map for display
+            loadFlightData(result);
+            passToMap(flightData);
+        });
+    }
+    function loadResultsTable (result) {
+        // load the 'resultTable' with the key value pairs of the 'result' object from the Query response
+        for (let i = 0; i < result.length; i++) {
+            setResultTable(resultTable => [...resultTable, {
+                flightId: result[i].flightId,
+                callsign: result[i].callsign,
+                aircraft: result[i].aircraft,
+                icao24: result[i].icao24,
+                departureAirport: result[i].departureAirport,
+                arrivalAirport: result[i].arrivalAirport,
+                departureDateTime: result[i].departureDateTime,
+                arrivalDateTime: result[i].arrivalDateTime,
+                flight_duration: result[i].flight_duration
+            }]);
+        }
     }
     //-----------------------------------------------------------------------------------------------------------------
+    // Map functions & variables |
+    //----------------------------
+    const [flightData, setFlightData] = useState([]);
+    function loadFlightData (result) {
+        // load the 'flightData' with the key value pairs of the 'result' object from the Query response
+        for (let i = 0; i < result.length; i++) {
+            setFlightData(flightData => [...flightData, {
+                //TODO: Change these to include waypoints etc...
+                /*flightId: result[i].flightId,
+                callsign: result[i].callsign,
+                aircraft: result[i].aircraft,
+                icao24: result[i].icao24,
+                departureAirport: result[i].departureAirport,
+                arrivalAirport: result[i].arrivalAirport,
+                departureDateTime: result[i].departureDateTime,
+                arrivalDateTime: result[i].arrivalDateTime,
+                flight_duration: result[i].flight_duration*/
+            }]);
+        }
+    }
+    function passToMap (result) {
+        //TODO: Pass the flightData to the map -- create required MapContainer functions&variables
+    }
 
+    //-----------------------------------------------------------------------------------------------------------------
     return(
         <body>
             <center id="search--title">Search Menu</center>
-            {/* Build onSubmit in findFlights - Create states and pass props down in SearchMenu*/}
             <SearchMenu placeholder={"Enter an Airfield..."} data = {airportData} onFormSubmit={handleFormSubmit} />
             <MapContainer />
             { showPopup && <ResultsPopup
                 handleClose={togglePopup}
+                handleLoad={handleLoad}
                 content={
-                    <table>
+                    <table id = "resultTable">
                         <tr>
                             <th>Selected</th>
                             <th>Call Sign</th>
@@ -54,19 +124,21 @@ function Findflights (){
                             <th>ICAO24</th>
                             <th>Departure</th>
                             <th>Arrival</th>
-                            <th>Departure Time</th>
-                            <th>Arrival Time</th>
+                            <th>Departure DateTime</th>
+                            <th>Arrival DateTime</th>
+                            <th>Flight Duration</th>
                         </tr>
                         {resultTable.map((flight, index) => (
-                            <tr>
-                                <td><input type="checkbox" name="selected" value="selected"/></td>
+                            <tr >
+                                <td><input type="checkbox" name="selected" value={flight.flightId} onChange={handleCheck}/></td>
                                 <td>{flight.callsign}</td>
                                 <td>{flight.aircraft}</td>
                                 <td>{flight.icao24}</td>
-                                <td>{flight.departure}</td>
-                                <td>{flight.arrival}</td>
-                                <td>{flight.departureTime}</td>
-                                <td>{flight.arrivalTime}</td>
+                                <td>{flight.departureAirport}</td>
+                                <td>{flight.arrivalAirport}</td>
+                                <td>{flight.departureDateTime}</td>
+                                <td>{flight.arrivalDateTime}</td>
+                                <td>{flight.flight_duration}</td>
                             </tr>
                         ))}
                     </table>
